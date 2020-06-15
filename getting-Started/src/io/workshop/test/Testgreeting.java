615c08d6f332df97b30abe7c20ba76509afefe49
Haneef <haneefshiraz1@gmail.com>
2020-06-15 22:25:17 +0530

    created main class

---
+++ getting-Started/src/io/workshop/test/Testgreeting.java
@@ -0,0 +1,13 @@
+package io.workshop.test;
+
+import io.workshop.domain.Greeting;
+
+public class Testgreeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting hello = new Greeting();
+		hello.greet();
+	}
+
+}

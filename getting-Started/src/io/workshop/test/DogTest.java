a82c3443ddeda0df4bb838b92134399637d988b1
Haneef <haneefshiraz1@gmail.com>
2020-06-15 22:56:43 +0530

    create dogtest main method

---
+++ getting-Started/src/io/workshop/test/DogTest.java
@@ -0,0 +1,16 @@
+package io.workshop.test;
+
+import io.workshop.domain.dog;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		dog hello=new dog();
+		hello.setweight(42);
+		System.out.println("dog weight is "+ hello.getweight());
+				
+	}
+
+}
